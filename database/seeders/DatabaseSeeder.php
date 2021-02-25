6569e08622bb17beafeb1d226ece7e4c0c66a40f
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-02-25 17:03:00 +0100

    [add] seeder for all models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Classroom;
+use App\Models\Course;
+use App\Models\Mark;
+use App\Models\Speaker;
+use App\Models\Student;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +19,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        DB::table('classrooms')->truncate();
+        DB::table('speakers')->truncate();
+        DB::table('students')->truncate();
+        DB::table('courses')->truncate();
+        DB::table('marks')->truncate();
+
+        Classroom::factory(10)->create();
+        Speaker::factory(20)->create();
+        Student::factory(100)->create();
+        Course::factory(40)->create();
+        Mark::factory(300)->create();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
