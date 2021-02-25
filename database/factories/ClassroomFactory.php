2a3a1026faae903c5d57e3688d8979eb4e4a13c4
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-02-25 16:59:21 +0100

    [add] factory for classrooms

---
+++ database/factories/ClassroomFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Classroom;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ClassroomFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Classroom::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => 'Classe ' . $this->faker->numberBetween($min = 1, $max = 100),
+            'promotion_date' => $this->faker->numberBetween($min = 2020, $max = 2026),
+        ];
+    }
+}
