beaac6a6028c6c8b1f09e523cc6506cadffcc12c
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-02-25 17:00:49 +0100

    [add] factory for speaker

---
+++ database/factories/SpeakerFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Speaker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SpeakerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Speaker::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'year' => $this->faker->numberBetween($min = 2015, $min = 2021)
+        ];
+    }
+}
