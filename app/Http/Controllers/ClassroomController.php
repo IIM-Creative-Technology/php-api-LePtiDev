2df16ca1e96703a8d8c37031d3c4a0b1155fb324
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-02-27 18:22:05 +0100

    [add] all methods for the classrooms api

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Classroom;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ClassroomController extends Controller
 {
@@ -11,23 +12,97 @@ class ClassroomController extends Controller
         return Classroom::all();
     }
 
-    /**
-     * Store a new user.
-     *
-     * @param Request $request
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
-     */
+    public function indexStudents($id){
 
-    public function show(Request $request, $id){
-
-        if(Classroom::find(19)){
-            return Classroom::find(19);
+        if(Classroom::find($id)){
+            $classroom = Classroom::find($id);
+            return $classroom->students;
         }
+
         return response([
             "error" => 404,
-            "message" => "L'id transmit n'existe pas",
-            "test" => $request,
-            "test2" => $id
+            "message" => "L'id transmit n'existe pas"
         ] , 404);
+
+    }
+
+    public function show($id){
+
+        if(Classroom::find($id)){
+            return Classroom::find($id);
+        }
+
+        return response([
+            "error" => 404,
+            "message" => "L'id transmit n'existe pas"
+        ] , 404);
+
+    }
+
+    public function create(Request $request){
+
+        $classroom = new Classroom();
+        $input = $request->input();
+
+        if (isset($input["name"]) || isset($input["promotion_date"])){
+
+            return response([
+                "error" => 422,
+                "message" => "La requette n'est pas bonne",
+            ] , 422);
+
+        }
+
+        elseif(DB::table("classrooms")->where('name', $input["name"])->first()){
+
+            return response([
+                "error" => 303,
+                "message" => "La classe existe déjà",
+            ] , 303);
+
+        }
+
+        else{
+            $classroom->fill($input)->save();
+
+            return response([
+                "error" => 200,
+                "message" => "La classe a été créée",
+            ] , 200);
+
+        }
+    }
+
+    public function update(Request $request){
+        $input = $request->input();
+
+        if(Classroom::find($input["id"])){
+
+            $classroom = Classroom::find($input["id"]);
+            $classroom->fill($input)->save();
+
+            return response([
+                "error" => 200,
+                "message" => "Classe bien modifier",
+            ] , 200);
+
+        }
+        elseif(!isset($input["id"]) || !isset($input["name"]) || !isset($input["promotion_date"])){
+
+            return response([
+                "error" => 422,
+                "message" => "La requette n'est pas bonne",
+            ] , 422);
+
+        }
+        else{
+
+            return response([
+                "error" => 404,
+                "message" => "La classe n'existe pas",
+            ] , 404);
+
+        }
+
     }
 }
