de430feb6cb3b83e3f85f49fb6ce585337548937
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-03-13 16:51:35 +0100

    [update] jwt middleware

--- routes/api.php
+++ routes/api.php
@@ -18,48 +18,62 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
 
-
 /*
- * Classroom Route
- */
-Route::get('/classrooms', [\App\Http\Controllers\ClassroomController::class, 'index']);
-Route::get('/classrooms/{id}', [\App\Http\Controllers\ClassroomController::class, 'show']);
-Route::get('/classrooms/students/{id}', [\App\Http\Controllers\ClassroomController::class, 'indexStudents']);
-Route::post('/classrooms', [\App\Http\Controllers\ClassroomController::class, 'create']);
-Route::put('/classrooms', [\App\Http\Controllers\ClassroomController::class, 'update']);
+* Auth Middleware
+*/
+
+Route::group([
+    "middleware" => 'api',
+    "prefix" => 'auth',
+], function(){
+    Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'],);
+});
+
+Route::middleware('jwt.auth')->group(function(){
+
+    /*
+    * Classroom Route
+    */
+    Route::get('/classrooms', [\App\Http\Controllers\ClassroomController::class, 'index']);
+    Route::get('/classrooms/{id}', [\App\Http\Controllers\ClassroomController::class, 'show']);
+    Route::get('/classrooms/students/{id}', [\App\Http\Controllers\ClassroomController::class, 'indexStudents']);
+    Route::post('/classrooms', [\App\Http\Controllers\ClassroomController::class, 'create']);
+    Route::put('/classrooms', [\App\Http\Controllers\ClassroomController::class, 'update']);
 
 
-/*
- * Students Route
- */
-Route::get('/students', [\App\Http\Controllers\StudentController::class, 'index']);
-Route::get('/students/{id}', [\App\Http\Controllers\StudentController::class, 'show']);
-Route::get('/students/marks/{id}', [\App\Http\Controllers\StudentController::class, 'indexMarks']);
-Route::post('/students', [\App\Http\Controllers\StudentController::class, 'create']);
-Route::put('/students', [\App\Http\Controllers\StudentController::class, 'update']);
-Route::delete('/students/{id}', [\App\Http\Controllers\StudentController::class, 'destroy']);
+    /*
+    * Students Route
+    */
+    Route::get('/students', [\App\Http\Controllers\StudentController::class, 'index']);
+    Route::get('/students/{id}', [\App\Http\Controllers\StudentController::class, 'show']);
+    Route::get('/students/marks/{id}', [\App\Http\Controllers\StudentController::class, 'indexMarks']);
+    Route::post('/students', [\App\Http\Controllers\StudentController::class, 'create']);
+    Route::put('/students', [\App\Http\Controllers\StudentController::class, 'update']);
+    Route::delete('/students/{id}', [\App\Http\Controllers\StudentController::class, 'destroy']);
 
 
-/*
- * Speakers Route
- */
-Route::get('/speakers', [\App\Http\Controllers\SpeakerController::class, 'index']);
-Route::get('/speakers/{id}', [\App\Http\Controllers\SpeakerController::class, 'show']);
-Route::post('/speakers', [\App\Http\Controllers\SpeakerController::class, 'create']);
-Route::put('/speakers', [\App\Http\Controllers\SpeakerController::class, 'update']);
+    /*
+    * Speakers Route
+    */
+    Route::get('/speakers', [\App\Http\Controllers\SpeakerController::class, 'index']);
+    Route::get('/speakers/{id}', [\App\Http\Controllers\SpeakerController::class, 'show']);
+    Route::post('/speakers', [\App\Http\Controllers\SpeakerController::class, 'create']);
+    Route::put('/speakers', [\App\Http\Controllers\SpeakerController::class, 'update']);
 
 
-/*
- * Courses Route
- */
-Route::get('/courses', [\App\Http\Controllers\CourseController::class, 'index']);
-Route::get('/courses/{id}', [\App\Http\Controllers\CourseController::class, 'show']);
-Route::post('/courses', [\App\Http\Controllers\CourseController::class, 'create']);
-Route::put('/courses', [\App\Http\Controllers\CourseController::class, 'update']);
+    /*
+    * Courses Route
+    */
+    Route::get('/courses', [\App\Http\Controllers\CourseController::class, 'index']);
+    Route::get('/courses/{id}', [\App\Http\Controllers\CourseController::class, 'show']);
+    Route::post('/courses', [\App\Http\Controllers\CourseController::class, 'create']);
+    Route::put('/courses', [\App\Http\Controllers\CourseController::class, 'update']);
 
 
-/*
- * Marks Route
- */
-Route::get('/marks/students/{id}', [\App\Http\Controllers\MarkController::class, 'show']);
-Route::post('/marks', [\App\Http\Controllers\MarkController::class, 'create']);
+    /*
+    * Marks Route
+    */
+    Route::get('/marks/students/{id}', [\App\Http\Controllers\MarkController::class, 'show']);
+    Route::post('/marks', [\App\Http\Controllers\MarkController::class, 'create']);
+
+});
